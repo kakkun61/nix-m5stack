9497d6c8a430ee33e2a3b3073fe984148cbb36bb
Kazuki Okamoto <kakkun61@gmail.com>
2024-01-04 06:18:40 +0900

    なんかこんなかんじ

--- m5gfx/flake.nix
+++ m5gfx/flake.nix
@@ -7,22 +7,35 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        name = "m5gfx";
       in
       {
-        inherit name;
-        packages.default = pkgs.stdenv.mkDerivation (finalAttrs: {
-          pname = name;
-          version = "0.1.12";
-          src = pkgs.fetchurl {
-            url = "https://github.com/m5stack/M5GFX/archive/refs/tags/${finalAttrs.version}.tar.gz";
-            hash = "sha256-TRlDglyB4SXc2uQBR7i0EKEvCV7NswrhblskUJIp5BU=";
-          };
-          installPhase = ''
-            mkdir -p $out/include
-            cp -r src/* $out/include
-          '';
-        });
+        name = "m5stack";
+        packages = {
+          m5stack = pkgs.stdenv.mkDerivation (finalAttrs: {
+            pname = "m5stack";
+            version = "0.7.1";
+            src = pkgs.fetchurl {
+              url = "https://github.com/m5stack/M5Stack/archive/refs/tags/${finalAttrs.version}.tar.gz";
+              hash = "sha256-TRlDglyB4SXc2uQBR7i0EKEvCV7NswrhblskUJIp5BU=";
+            };
+            installPhase = ''
+              mkdir -p $out/include
+              cp -r src/* $out/include
+            '';
+          });
+          m5gfx = pkgs.stdenv.mkDerivation (finalAttrs: {
+            pname = "m5gfx";
+            version = "0.1.12";
+            src = pkgs.fetchurl {
+              url = "https://github.com/m5stack/M5GFX/archive/refs/tags/${finalAttrs.version}.tar.gz";
+              hash = "sha256-TRlDglyB4SXc2uQBR7i0EKEvCV7NswrhblskUJIp5BU=";
+            };
+            installPhase = ''
+              mkdir -p $out/include
+              cp -r src/* $out/include
+            '';
+          });
+        };
         formatter = pkgs.nixpkgs-fmt;
       }
     );
